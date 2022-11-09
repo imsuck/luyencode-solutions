06f440e17cb97e2832a28d7871143032a831469d
imsuck <imsuck12@gmail.com>
2022-11-09 11:28:03 +0700

    Make HCNV2 work

--- HCNV2.cpp
+++ HCNV2.cpp
@@ -1,40 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-unsigned long long diff(int x, int y) { return abs(abs(x) - abs(y)); }
+#define lol long long
+
+lol diff(lol x, lol y) { return max(x, y) - min(x, y); }
+
+lol max_4(lol x, lol y, lol z, lol w) { return max(max(x, y), max(z, w)); }
+lol min_4(lol x, lol y, lol z, lol w) { return min(min(x, y), min(z, w)); }
 
 int main() {
     int ax1, ay1, ax2, ay2;
     cin >> ax1 >> ay1 >> ax2 >> ay2;
     int bx1, by1, bx2, by2;
     cin >> bx1 >> by1 >> bx2 >> by2;
-    unsigned long long area;
+    lol area;
 
-    // sort
-    if (ax1 > ax2)
-        swap(ax1, ax2);
-    if (ay1 > ay2)
-        swap(ay1, ay2);
-    if (bx1 > bx2)
-        swap(bx1, bx2);
-    if (by1 > by2)
-        swap(by1, by2);
-
-    unsigned long long diff_x = 0;
-    unsigned long long diff_y = 0;
-
-    if (ax1 <= bx1 && bx1 < ax2) {
-        diff_x = diff(bx1, min(ax2, bx2));
-    }
-    if (ax1 < bx2 && bx2 <= ax2) {
-        diff_x = diff(max(ax1, bx1), bx2);
-    }
-    if (ay1 <= by1 && by1 < ay2) {
-        diff_y = diff(by1, min(ay2, by2));
-    }
-    if (ay1 < by2 && by2 <= ay2) {
-        diff_y = diff(max(ay1, by1), by2);
-    }
+    lol diff_x =
+        max(0LL, (diff(ax1, ax2) + diff(bx1, bx2) - max_4(ax1, ax2, bx1, bx2) +
+                  min_4(ax1, ax2, bx1, bx2)));
+    lol diff_y =
+        max(0LL, (diff(ay1, ay2) + diff(by1, by2) - max_4(ay1, ay2, by1, by2) +
+                  min_4(ay1, ay2, by1, by2)));
 
     area = diff_x * diff_y;
 
